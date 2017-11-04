774fb664148b475ed71dcef65b9877d5d2c6cef8
sevgin0954 <skateboardista_0954@abv.bg>
2017-11-04 09:38:16 +0200

    add 08. Increase Minion Age to FETCHING RESULTSETS WITH ADO.NET

---
+++ Databases Advanced - Entity Framework/FETCHING RESULTSETS WITH ADO.NET/08. Increase Minion Age/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        int[] ids = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        SqlConnectionStringBuilder connectionString = new SqlConnectionStringBuilder()
+        {
+            ["Server"] = "DESKTOP-4FKVBUR\\SQLEXPRESS",
+            ["Database"] = "MinionsDB",
+            ["Integrated Security"] = "true"
+        };
+        SqlConnection connection = new SqlConnection(connectionString.ToString());
+
+        connection.Open();
+        using (connection)
+        {
+            for (int a = 0; a < ids.Length; a++)
+            {
+                string query =
+                "UPDATE Minions " +
+                "SET Age += 1, Name = UPPER(Name) " +
+                $"WHERE Id = @Id";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Id", ids[a]);
+                command.ExecuteNonQuery();
+            }
+
+            PrintDatabase("Minions", connection);
+        }
+    }
+
+    static void PrintDatabase(string database, SqlConnection connection)
+    {
+        using (connection)
+        {
+            string query =
+                $"SELECT Name, Age FROM {database}";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["Name"] + " " + reader["Age"]);
+            }
+        }
+    }
+}
