07407a99f25a358ed473887a86fbf9a7f47b6cf4
sevgin0954 <skateboardista_0954@abv.bg>
2018-02-05 15:27:26 +0200

    add new solutions

---
+++ C# Advanced/CSharp Advanced Exam Retake - 3 September 2017/02. Crypto Master/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = Console.ReadLine()
+            .Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        int bestLenIncreasingSeq = 1;
+        int jumpCount = 1;
+
+        while (jumpCount < arr.Length)
+        {   
+            for (int startIndex = 0; startIndex < arr.Length; startIndex++)
+            {
+                int currentIndex = startIndex;
+                int nextIndex = currentIndex + jumpCount;
+                int currentIncreasingSeqLen = 1;
+
+                while (true)
+                {
+                    if (nextIndex >= arr.Length)
+                    {
+                        nextIndex -= arr.Length;
+                    }
+                    if (arr[currentIndex] < arr[nextIndex])
+                    {
+                        currentIncreasingSeqLen++;
+                        if (currentIncreasingSeqLen > bestLenIncreasingSeq)
+                        {
+                            bestLenIncreasingSeq = currentIncreasingSeqLen;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    currentIndex = nextIndex;
+                    nextIndex += jumpCount;
+                }
+            }
+
+            jumpCount++;
+        }
+
+        Console.WriteLine(bestLenIncreasingSeq);
+    }
+}
